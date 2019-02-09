bb682cdf9600fcc1d5c7604084b26cb6bf33d220
MH.Movasaghi <mmghho77@gmail.com>
2019-02-09 19:46:44 +0330

    Init Project

---
+++ WindowsFormsDooz/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsDooz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(17, 16);
+            this.btn1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(171, 153);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(203, 16);
+            this.btn2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(171, 153);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(17, 183);
+            this.btn4.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(171, 153);
+            this.btn4.TabIndex = 2;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(390, 16);
+            this.btn3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(171, 153);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(203, 183);
+            this.btn5.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(171, 153);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(390, 183);
+            this.btn6.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(171, 153);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(17, 350);
+            this.btn7.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(171, 153);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(203, 350);
+            this.btn8.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(171, 153);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(390, 350);
+            this.btn9.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(171, 153);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 517);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 72);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(297, 517);
+            this.button4.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(269, 72);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Result";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 619);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(619, 707);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DOOZ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsDooz/Form1.cs
@@ -0,0 +1,448 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDooz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int PlayerOne = 0, Pc = 0;
+        Int32 Check()
+        {
+            if(btn1.Text==btn2.Text && btn2.Text==btn3.Text)
+            {
+                if (btn1.Text == "X")
+                    return 1;
+                if (btn1.Text == "O")
+                    return 2;
+            }
+            if (btn1.Text == btn4.Text && btn4.Text == btn7.Text)
+            {
+                if (btn1.Text == "X")
+                    return 1;
+                if (btn1.Text == "O")
+                    return 2;
+            }
+            if (btn1.Text == btn5.Text && btn5.Text == btn9.Text)
+            {
+                if (btn1.Text == "X")
+                    return 1;
+                if (btn1.Text == "O")
+                    return 2;
+            }
+            if (btn2.Text == btn5.Text && btn5.Text == btn8.Text)
+            {
+                if (btn2.Text == "X")
+                    return 1;
+                if (btn2.Text == "O")
+                    return 2;
+            }
+            if (btn3.Text == btn6.Text && btn6.Text == btn9.Text)
+            {
+                if (btn3.Text == "X")
+                    return 1;
+                if (btn3.Text == "O")
+                    return 2;
+            }
+            if (btn3.Text == btn5.Text && btn5.Text == btn7.Text)
+            {
+                if (btn3.Text == "X")
+                    return 1;
+                if (btn3.Text == "O")
+                    return 2;
+            }
+            if (btn4.Text == btn5.Text && btn5.Text == btn6.Text)
+            {
+                if (btn4.Text == "X")
+                    return 1;
+                if (btn4.Text == "O")
+                    return 2;
+            }
+            if (btn7.Text == btn8.Text && btn8.Text == btn9.Text)
+            {
+                if (btn7.Text == "X")
+                    return 1;
+                if (btn7.Text == "O")
+                    return 2;
+            }
+            return 0;
+        }
+        void NewGame()
+        {
+            btn1.Text = " ";
+            btn1.Enabled = true;
+            btn2.Text = " ";
+            btn2.Enabled = true;
+            btn3.Text = " ";
+            btn3.Enabled = true;
+            btn4.Text = " ";
+            btn4.Enabled = true;
+            btn5.Text = " ";
+            btn5.Enabled = true;
+            btn6.Text = " ";
+            btn6.Enabled = true;
+            btn7.Text = " ";
+            btn7.Enabled = true;
+            btn8.Text = " ";
+            btn8.Enabled = true;
+            btn9.Text = " ";
+            btn9.Enabled = true;
+            
+        }
+        void PcSelect(Button btn)
+        {
+            btn.Text = "O";
+            btn.Enabled = false;
+        }
+        void PcPlay()
+        {
+
+            if (btn1.Text=="O" && btn2.Text=="O" && btn3.Enabled)
+            {
+                PcSelect(btn3);
+            }
+            else if (btn2.Text == "O" && btn3.Text == "O" && btn1.Enabled)
+            {
+                PcSelect(btn1);
+            }
+            else if (btn1.Text == "O" && btn3.Text == "O" && btn1.Enabled)
+            {
+                PcSelect(btn2);
+            }
+            else if (btn4.Text == "O" && btn5.Text == "O" && btn6.Enabled)
+            {
+                PcSelect(btn6);
+            }
+            else if (btn4.Text == "O" && btn6.Text == "O" && btn5.Enabled)
+            {
+                PcSelect(btn5);
+            }
+            else if (btn5.Text == "O" && btn6.Text == "O" && btn4.Enabled)
+            {
+                PcSelect(btn4);
+            }
+            else if (btn7.Text == "O" && btn8.Text == "O" && btn9.Enabled)
+            {
+                PcSelect(btn9);
+            }
+            else if (btn7.Text == "O" && btn9.Text == "O" && btn8.Enabled)
+            {
+                PcSelect(btn8);
+            }
+            else if (btn8.Text == "O" && btn9.Text == "O" && btn7.Enabled)
+            {
+                PcSelect(btn7);
+            }
+            else if (btn1.Text == "O" && btn4.Text == "O" && btn7.Enabled)
+            {
+                PcSelect(btn7);
+            }
+            else if (btn1.Text == "O" && btn7.Text == "O" && btn4.Enabled)
+            {
+                PcSelect(btn4);
+            }
+            else if (btn4.Text == "O" && btn7.Text == "O" && btn1.Enabled)
+            {
+                PcSelect(btn1);
+            }
+            else if (btn2.Text == "O" && btn5.Text == "O" && btn8.Enabled)
+            {
+                PcSelect(btn8);
+            }
+            else if (btn2.Text == "O" && btn8.Text == "O" && btn5.Enabled)
+            {
+                PcSelect(btn5);
+            }
+            else if (btn8.Text == "O" && btn5.Text == "O" && btn2.Enabled)
+            {
+                PcSelect(btn2);
+            }
+            else if (btn3.Text == "O" && btn6.Text == "O" && btn9.Enabled)
+            {
+                PcSelect(btn9);
+            }
+            else if (btn3.Text == "O" && btn9.Text == "O" && btn6.Enabled)
+            {
+                PcSelect(btn6);
+            }
+            else if (btn6.Text == "O" && btn9.Text == "O" && btn3.Enabled)
+            {
+                PcSelect(btn3);
+            }
+            else if (btn1.Text == "O" && btn5.Text == "O" && btn9.Enabled)
+            {
+                PcSelect(btn9);
+            }
+            else if (btn5.Text == "O" && btn9.Text == "O" && btn1.Enabled)
+            {
+                PcSelect(btn1);
+            }
+            else if (btn1.Text == "O" && btn9.Text == "O" && btn5.Enabled)
+            {
+                PcSelect(btn5);
+            }
+            else if (btn3.Text == "O" && btn5.Text == "O" && btn7.Enabled)
+            {
+                PcSelect(btn7);
+            }
+            else if (btn5.Text == "O" && btn7.Text == "O" && btn3.Enabled)
+            {
+                PcSelect(btn3);
+            }
+            else if (btn3.Text == "O" && btn7.Text == "O" && btn5.Enabled)
+            {
+                PcSelect(btn5);
+            }
+            else if (btn1.Text == "X" && btn2.Text == "X" && btn3.Enabled)
+            {
+                PcSelect(btn3);
+            }
+            else if (btn2.Text == "X" && btn3.Text == "X" && btn1.Enabled)
+            {
+                PcSelect(btn1);
+            }
+            else if (btn1.Text == "X" && btn3.Text == "X" && btn1.Enabled)
+            {
+                PcSelect(btn2);
+            }
+            else if (btn4.Text == "X" && btn5.Text == "X" && btn6.Enabled)
+            {
+                PcSelect(btn6);
+            }
+            else if (btn4.Text == "X" && btn6.Text == "X" && btn5.Enabled)
+            {
+                PcSelect(btn5);
+            }
+            else if (btn5.Text == "X" && btn6.Text == "X" && btn4.Enabled)
+            {
+                PcSelect(btn4);
+            }
+            else if (btn7.Text == "X" && btn8.Text == "X" && btn9.Enabled)
+            {
+                PcSelect(btn9);
+            }
+            else if (btn7.Text == "X" && btn9.Text == "X" && btn8.Enabled)
+            {
+                PcSelect(btn8);
+            }
+            else if (btn8.Text == "X" && btn9.Text == "X" && btn7.Enabled)
+            {
+                PcSelect(btn7);
+            }
+            else if (btn1.Text == "X" && btn4.Text == "X" && btn7.Enabled)
+            {
+                PcSelect(btn7);
+            }
+            else if (btn1.Text == "X" && btn7.Text == "X" && btn4.Enabled)
+            {
+                PcSelect(btn4);
+            }
+            else if (btn4.Text == "X" && btn7.Text == "X" && btn1.Enabled)
+            {
+                PcSelect(btn1);
+            }
+            else if (btn2.Text == "X" && btn5.Text == "X" && btn8.Enabled)
+            {
+                PcSelect(btn8);
+            }
+            else if (btn2.Text == "X" && btn8.Text == "X" && btn5.Enabled)
+            {
+                PcSelect(btn5);
+            }
+            else if (btn8.Text == "X" && btn5.Text == "X" && btn2.Enabled)
+            {
+                PcSelect(btn2);
+            }
+            else if (btn3.Text == "X" && btn6.Text == "X" && btn9.Enabled)
+            {
+                PcSelect(btn9);
+            }
+            else if (btn3.Text == "X" && btn9.Text == "X" && btn6.Enabled)
+            {
+                PcSelect(btn6);
+            }
+            else if (btn6.Text == "X" && btn9.Text == "X" && btn3.Enabled)
+            {
+                PcSelect(btn3);
+            }
+            else if (btn1.Text == "X" && btn5.Text == "X" && btn9.Enabled)
+            {
+                PcSelect(btn9);
+            }
+            else if (btn5.Text == "X" && btn9.Text == "X" && btn1.Enabled)
+            {
+                PcSelect(btn1);
+            }
+            else if (btn1.Text == "X" && btn9.Text == "X" && btn5.Enabled)
+            {
+                PcSelect(btn5);
+            }
+            else if (btn3.Text == "X" && btn5.Text == "X" && btn7.Enabled)
+            {
+                PcSelect(btn7);
+            }
+            else if (btn5.Text == "X" && btn7.Text == "X" && btn3.Enabled)
+            {
+                PcSelect(btn3);
+            }
+            else if (btn3.Text == "X" && btn7.Text == "X" && btn5.Enabled)
+            {
+                PcSelect(btn5);
+            }
+            else
+            {
+                bool Go = true;
+                if (!Equalaize())
+                {
+                    do
+                    {
+                        Random rnd = new Random();
+                        int num = (rnd.Next(1, 9));
+                        switch (num)
+                        {
+                            case 1:
+                                if (btn1.Enabled)
+                                {
+                                    PcSelect(btn1);
+                                    Go = false;
+                                }
+                                break;
+                            case 2:
+                                if (btn2.Enabled)
+                                {
+                                    PcSelect(btn2);
+                                    Go = false;
+                                }
+                                break;
+                            case 3:
+                                if (btn3.Enabled)
+                                {
+                                    PcSelect(btn3);
+                                    Go = false;
+                                }
+                                break;
+                            case 4:
+                                if (btn4.Enabled)
+                                {
+                                    PcSelect(btn4);
+                                    Go = false;
+                                }
+                                break;
+                            case 5:
+                                if (btn5.Enabled)
+                                {
+                                    PcSelect(btn5);
+                                    Go = false;
+                                }
+                                break;
+                            case 6:
+                                if (btn6.Enabled)
+                                {
+                                    PcSelect(btn6);
+                                    Go = false;
+                                }
+                                break;
+                            case 7:
+                                if (btn7.Enabled)
+                                {
+                                    PcSelect(btn7);
+                                    Go = false;
+                                }
+                                break;
+                            case 8:
+                                if (btn8.Enabled)
+                                {
+                                    PcSelect(btn8);
+                                    Go = false;
+                                }
+                                break;
+                            case 9:
+                                if (btn9.Enabled)
+                                {
+                                    PcSelect(btn9);
+                                    Go = false;
+                                }
+                                break;
+                        }
+                    } while (Go);
+                }
+            }
+        }
+
+        bool Equalaize()
+        {
+            if(btn1.Enabled || btn2.Enabled || btn3.Enabled || btn4.Enabled || btn5.Enabled || btn6.Enabled || btn7.Enabled || btn8.Enabled || btn9.Enabled)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void Click(object sender, EventArgs e)
+        {
+
+
+
+            Button button = (Button)sender;
+            button.Text = "X";
+            button.Enabled = false;
+
+
+            if(Check()!=0)
+            {
+                if(Check()==1)
+                {
+                    MessageBox.Show("YOU WIN :)","FINISH",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    PlayerOne++;
+                }
+                    
+                NewGame();
+            }
+
+
+            PcPlay();
+
+            if (Check() != 0)
+            {
+                if (Check() == 2)
+                {
+                    MessageBox.Show("YOU LOSE :(", "FINISH", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Pc++;
+                }
+
+                NewGame();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pc Point : " + Pc +"\nPlayer Point : " + PlayerOne , "RESULT",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+    }
+}
